8e8bf85f0f67de0aba02eef0ba2d36ed89246c3c
PraptektoGIT <praptekto@gmail.com>
2021-01-10 19:51:28 +0700

    latihan tentang response dan request kelar

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const http=require('http');
+const fis=require('fs');
+
 
 const server=http.createServer((req,res)=>{
   console.log(req.url, req.method);
 
-  //ada 3 step : 1. ser header 2. set yang mau dikirm 3. ending
   //set header content type
   res.setHeader('content-type','text/html');
 
-  res.write("<p>makan</p>");
-  res.write('<p>nasi</p>');
-  res.end();
+  //
+  fis.readFile('./views/index.html',(err,data)=>{
+    if(err){
+      console.log(err);
+      res.end();
+    }else{
+      res.write(data);
+      res.end();
+      //dua code diatas pas ini dapat juga ditulis dengan res.end(data);
+    }
 
+  });
 });
 
 server.listen(3000,'localhost',()=>{
