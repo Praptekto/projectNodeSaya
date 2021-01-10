53191bb04e02362654d7aee64df22074840d4168
PraptektoGIT <praptekto@gmail.com>
2021-01-10 18:31:03 +0700

    bagian response object kelar, pindah ke latihan4bagian2

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-  console.log('request has been made');
+  console.log(req.url, req.method);
+
+  //ada 3 step : 1. ser header 2. set yang mau dikirm 3. ending
+  //set header content type
+  res.setHeader('content-type','text/html');
+
+  res.write("<p>makan</p>");
+  res.write('<p>nasi</p>');
+  res.end();
+
 });
 
 server.listen(3000,'localhost',()=>{
