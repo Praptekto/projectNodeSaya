8d4c079bff1775d2fbf6dafff14172583c42e849
PraptektoGIT <praptekto@gmail.com>
2021-01-11 21:18:42 +0700

    view engine kelar

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express=require('express');
 const app=express();
 
+//register view engine
+app.set('view engine','ejs');// membuat view enginenay menjadi ejs
+//app.set('views','foldertempat ejs');, deafaulnya ke folder views
+
 //listen for request
 app.listen(3000);
 
-app.get('',(req,res)=>{
+app.get('/',(req,res)=>{
+  const blogs=[
+    {judul: 'Makan nasi',isi:'ini adalah isi didalam blognya'},
+    {judul: 'Makan nasi',isi:'ini adalah isi didalam blognya'},
+    {judul: 'Makan nasi',isi:'ini adalah isi didalam blognya'},
+    {judul: 'Makan nasi',isi:'ini adalah isi didalam blognya'}
+  ];
   //res.send('<p>Mantap</p>')
   //cara mengirim file
-  res.sendFile('./views/index.html',{root: __dirname});
+  //res.sendFile('index');
+  res.render('index',{title:'home',blogs}); //res nya render, bukan sendfile
 });
-
 app.get('/about',(req,res)=>{
   //res.send('<p>Mantap</p>')
   //cara mengirim file
-  res.sendFile('./views/about.html',{root: __dirname});
+  res.render('about',{tentang:"tentang"});
 });
-
-//redirect
-app.get('/about-me',(req,res)=>{
-  res.redirect('/about');
+app.get('/blogs/create',(req,res)=>{//directory ini diterima dari tag href
+  //res.send('<p>Mantap</p>')
+  //cara mengirim file
+  //res.sendFile('index');
+  res.render('create',{buat:"buat blog"}); //res nya render, bukan sendfile
 });
-
 //================================================================
 //404 not found
 //use ini mirip dengan defaulnya switch
@@ -28,5 +38,5 @@ app.get('/about-me',(req,res)=>{
 app.use((req,res)=>{
  //res.sendFile('./views/404.html',{root: __dirname});
  //VERSI 2
- res.status(404).sendFile('./views/404.html',{root: __dirname});
+ res.status(404).render('404',{notfound:"tidak ditemukan"});
 });
